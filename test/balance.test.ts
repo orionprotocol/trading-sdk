5c0e6d111d74806c3e680fdef1dc71bb13993f17
yevhen.t <peiko.tumakov.y@gmail.com>
2021-11-25 16:50:04 +0200

    fix: WS ping-pong handling in aggregator v2

--- test/balance.test.ts
+++ test/balance.test.ts
@@ -3,6 +3,8 @@ import { ethers } from 'ethers';
 import { Chain, Exchange } from '../src/index'
 import { NETWORK, TEST_WALLET } from '../src/utils/Constants'
 
+jest.setTimeout(40000)
+
 describe('Get balances', () => {
     let chain: Chain
     let exchange: Exchange
